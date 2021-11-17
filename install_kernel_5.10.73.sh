67c5f842a8fec93948b90d5bdb549c904adea259
Sergej Schumilo <sergej@schumilo.de>
2021-11-17 16:56:07 +0100

    update install_kernel_5.10.73.sh

--- install_kernel_5.10.73.sh
+++ install_kernel_5.10.73.sh
@@ -1,6 +1,6 @@
 cd /tmp/ &&
-wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.31/amd64/linux-headers-5.10.31-051031_5.10.31-051031.202104160635_all.deb &&
-wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.31/amd64/linux-headers-5.10.31-051031-generic_5.10.31-051031.202104160635_amd64.deb &&
-wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.31/amd64/linux-image-unsigned-5.10.31-051031-generic_5.10.31-051031.202104160635_amd64.deb &&
-wget -c https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.31/amd64/linux-modules-5.10.31-051031-generic_5.10.31-051031.202104160635_amd64.deb &&
+wget -c  https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.73/amd64/linux-headers-5.10.73-051073-generic_5.10.73-051073.202110131013_amd64.deb &&
+wget -c  https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.73/amd64/linux-headers-5.10.73-051073_5.10.73-051073.202110131013_all.deb &&
+wget -c  https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.73/amd64/linux-image-unsigned-5.10.73-051073-generic_5.10.73-051073.202110131013_amd64.deb &&
+wget -c  https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.73/amd64/linux-modules-5.10.73-051073-generic_5.10.73-051073.202110131013_amd64.deb &&
 sudo dpkg -i *.deb
